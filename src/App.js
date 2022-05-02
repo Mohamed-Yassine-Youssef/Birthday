ffd2e84f6f164891d538ace5184a3849b99d7077
Mohamed-Yassine-Youssef <yousssmed@gmail.com>
2022-05-02 19:19:02 +1200

    Little  React project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import List from "./components/List";
+import Data from "./components/Data";
+import { useState } from "react";
 function App() {
+  const [people, setPeople] = useState(Data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main>
+      <section className="container">
+        <h3>{people.length} birthday today</h3>
+        <List people={people} />
+        <button
+          onClick={() => {
+            setPeople([]);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Clear All
+        </button>
+      </section>
+    </main>
   );
 }
 
